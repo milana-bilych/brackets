bf6ab2429fe5630577b80a6051f4ae98c6761225
milana-bilych <akimmilana@gmail.com>
2022-08-05 20:40:12 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  let arr = bracketsConfig.map(item => item.join(''));
+
+  for (let i = 0; i < arr.length; i++) {
+    if (str.includes(arr[i])) {
+      str = str.replace(arr[i], '');
+      i = -1
+    }
+  }
+  return (str.length == 0);
 }
